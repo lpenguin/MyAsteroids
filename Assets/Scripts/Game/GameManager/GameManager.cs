f5d20eb6cba7395675c807993095918921ef97dd
Nikita <lpenguin@yandex.ru>
2022-04-16 09:06:41 +0300

    ScoreComponent, UI update

--- Assets/Scripts/Game/GameManager/GameManager.cs
+++ Assets/Scripts/Game/GameManager/GameManager.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using Game.Input;
+using Game.Music;
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
+using UnityEngine.InputSystem;
 
 namespace Game.GameManager
 {
+    // TODO: Singleton?
     public class GameManager: MonoBehaviour
     {
         [SerializeField]
@@ -17,30 +18,72 @@ namespace Game.GameManager
         private GameObject gameOverUi;
 
         [SerializeField]
-        private TMPro.TMP_Text scoreText;
+        private GameObject pauseUi;
 
+        private bool _isPaused;
+        
         private void Start()
         {
             gameOverUi.SetActive(false);
         }
 
+        public void Pause()
+        {
+            Time.timeScale = 0f;
+            pauseUi.SetActive(true);
+        }
+
+        public void Resume()
+        {
+            Time.timeScale = 1f;
+            pauseUi.SetActive(false);
+        }
+        
         public void RestartLevel()
         {
             Time.timeScale = 1f;
             SceneManager.LoadScene(SceneManager.GetActiveScene().path);
         }
         
+        public void Exit()
+        {
+            Application.Quit();
+        }
+
+        public void ChangeVolume(Single volume)
+        {
+            MusicManager.Instance.Volume = volume;
+        }
+
         private void OnEnable()
         {
+            playerState.audioListener = FindObjectOfType<AudioListener>();
+            
             Assert.IsNotNull(playerState, $"{nameof(playerState)} must be set");
             Assert.IsNotNull(gameOverUi, $"{nameof(gameOverUi)} must be set");
-            Assert.IsNotNull(scoreText, $"{nameof(scoreText)} must be set");
+            Assert.IsNotNull(playerState.audioListener, $"Cannot find {nameof(AudioListener)} in the scene");
+
             
             playerState.OnPlayerDeath += HandlePlayerDeath;
+            InputManager.Instance.Controls.Main.Pause.performed += OnPausePresed;
+        }
+
+        private void OnPausePresed(InputAction.CallbackContext obj)
+        {
+            _isPaused = !_isPaused;
+            if (_isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
         }
 
         private void OnDisable()
         {
+            InputManager.Instance.Controls.Main.Pause.performed -= OnPausePresed;
             playerState.OnPlayerDeath -= HandlePlayerDeath;
         }
         
@@ -48,7 +91,6 @@ namespace Game.GameManager
         {
             Time.timeScale = 0f;
             gameOverUi.SetActive(true);
-            scoreText.text = $"{playerState.playerData.Score:D}";
         }
 
     }
---
+++ Assets/Scripts/Game/UI/ScoreComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Game.GameManager;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Game.UI
+{
+    public class ScoreComponent: MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerState playerState;
+
+        private TMP_Text scoreText;
+        private void Start()
+        {
+            Assert.IsNotNull(playerState, $"{nameof(playerState)} must be set");
+            
+            scoreText = GetComponent<TMP_Text>();
+            Assert.IsNotNull(scoreText, $"{nameof(scoreText)} must be set");
+        }
+
+        private void Update()
+        {
+            if (scoreText != null &&
+                playerState != null)
+            {
+                scoreText.text = $"{playerState.playerData.Score:D}";
+            }
+        }
+    }
+}
