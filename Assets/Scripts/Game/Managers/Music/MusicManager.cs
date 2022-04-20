87335d0c40578d36b2ba86febba11d25d3c2818d
Nikita <lpenguin@yandex.ru>
2022-04-20 20:25:13 +0300

    MusicManager cleanup

--- Assets/Scripts/Game/Managers/Music/MusicManager.cs
+++ Assets/Scripts/Game/Managers/Music/MusicManager.cs
@@ -1,20 +1,14 @@
 ﻿using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace Game.Managers.Music
 {
     [RequireComponent(typeof(AudioSource))]
     public class MusicManager: MonoBehaviour, IMusicManager
     {
-        public static MusicManager Instance { get; private set; }
-
         private AudioSource audioSource;
         
         private void Awake()
         {
-            Assert.IsNull(Instance, $"{nameof(MusicManager)} singleton is already instantiated");
-            Instance = this;
-
             audioSource = GetComponent<AudioSource>();
         }
 
