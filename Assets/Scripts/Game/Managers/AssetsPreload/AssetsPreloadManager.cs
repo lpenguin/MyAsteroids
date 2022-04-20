310fcd0f7c69a6d7be51bede7fd0007289596c4e
Nikita <lpenguin@yandex.ru>
2022-04-20 20:39:19 +0300

    Preloading assets

---
+++ Assets/Scripts/Game/Managers/AssetsPreload/AssetsPreloadManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Game.Managers.AssetsPreload
+{
+    public class AssetsPreloadManager: MonoBehaviour
+    {
+        private const string PreloadAssetsKey = "Preload";
+        
+        private void Awake()
+        {
+            Addressables.LoadAssetAsync<GameObject>(PreloadAssetsKey);
+        }
+    }
+}
