ec255ba97c934e38594a017359676f650f6bd0af
Nikita <lpenguin@yandex.ru>
2022-04-21 13:33:00 +0300

    MusicManager: taking global music source

--- Assets/Scripts/Game/Managers/Music/MusicManager.cs
+++ Assets/Scripts/Game/Managers/Music/MusicManager.cs
@@ -1,29 +1,26 @@
 ﻿using Game.Managers.GameManager;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace Game.Managers.Music
 {
-    [RequireComponent(typeof(AudioSource))]
     public class MusicManager: MonoBehaviour, IMusicManager
     {
-        private AudioSource audioSource;
+        private IMusicSource _musicSource;
         
-        private void Awake()
+        private void OnEnable()
         {
-            audioSource = GetComponent<AudioSource>();
-            Assert.IsNotNull(audioSource, $"{nameof(audioSource)} must be set");
+            _musicSource = FindObjectOfType<MusicSource>();
         }
 
         private void Start()
         {
-            audioSource.volume = GameSingleton.Instance.PreferencesManager.MusicVolume;
+            _musicSource.Volume = GameSingleton.Instance.PreferencesManager.MusicVolume;
         }
 
         public float Volume
         {
-            get => audioSource.volume;
-            set => audioSource.volume = value;
+            get => _musicSource.Volume;
+            set => _musicSource.Volume = value;
         }
     }
 }
